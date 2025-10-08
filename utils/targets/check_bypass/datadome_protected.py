4b9bcd46c429da63cb60f12ed37444386291eeff
techinz <contact@techinz.dev>
2025-10-08 04:30:47 +0200

    Update datadome detection

--- utils/targets/check_bypass/datadome_protected.py
+++ utils/targets/check_bypass/datadome_protected.py
@@ -1,6 +1,9 @@
 import asyncio
+import logging
 
 from engines.base import BrowserEngine
+
+logger = logging.getLogger(__name__)
 
 
 async def check_datadome_bypass(engine: BrowserEngine, tries: int = 20) -> bool:
@@ -19,10 +22,26 @@ async def check_datadome_bypass(engine: BrowserEngine, tries: int = 20) -> bool:
         target_page_loaded_found, target_page_loaded_html = await engine.locator('#top')
         captcha_loaded_found, captcha_loaded_html = await engine.locator(
             'iframe[title*="Datadome"], iframe[title*="DataDome CAPTCHA"]')
+        captcha_loaded_found2, captcha_loaded_html2 = await engine.locator(
+            'iframe[title="DataDome Device Check"]')
 
-        if target_page_loaded_found or captcha_loaded_found:
-            bypass = not captcha_loaded_found
+        page_content = await engine.get_page_content()
+
+        captcha_loaded_found3, captcha_loaded_html3 = False, ''
+        if '<span>This site can’t be reached</span>' in page_content:
+            captcha_loaded_found3, captcha_loaded_html3 = True, '<span>This site can’t be reached</span>'
+
+        captcha_loaded_found4, captcha_loaded_html4 = False, ''
+        if '<body>' not in page_content:
+            captcha_loaded_found4, captcha_loaded_html4 = True, '<body> not in page content'
+
+        if any((target_page_loaded_found, captcha_loaded_found, captcha_loaded_found2,
+                captcha_loaded_html3, captcha_loaded_found4)):
+            bypass = not (
+                    captcha_loaded_found or captcha_loaded_found2 or captcha_loaded_html3 or captcha_loaded_found4)
             break
+
+        logger.info("Datadome bypass not determined yet, retrying...")
 
         await asyncio.sleep(2.5)
 
--- utils/targets/check_bypass/datadome_protected_2.py
+++ utils/targets/check_bypass/datadome_protected_2.py
@@ -1,6 +1,9 @@
 import asyncio
+import logging
 
 from engines.base import BrowserEngine
+
+logger = logging.getLogger(__name__)
 
 
 async def check_datadome2_bypass(engine: BrowserEngine, tries: int = 20) -> bool:
@@ -20,9 +23,17 @@ async def check_datadome2_bypass(engine: BrowserEngine, tries: int = 20) -> bool
         captcha_loaded_found, captcha_loaded_html = await engine.locator(
             '[id*="ddChallengeContainer"]')
 
-        if target_page_loaded_found or captcha_loaded_found:
-            bypass = not captcha_loaded_found
+        page_content = await engine.get_page_content()
+
+        captcha_loaded_found2, captcha_loaded_html2 = False, ''
+        if '<html><head><title>403 Forbidden</title></head>' in page_content:
+            captcha_loaded_found2, captcha_loaded_html2 = True, '<title>403 Forbidden</title>'
+
+        if any((target_page_loaded_found, captcha_loaded_found, captcha_loaded_found2)):
+            bypass = not (captcha_loaded_found or captcha_loaded_found2)
             break
+
+        logger.info("Datadome bypass 2 not determined yet, retrying...")
 
         await asyncio.sleep(2.5)
 
